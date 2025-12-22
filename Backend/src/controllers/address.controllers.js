803cb3e017395ba909b68fdd1c552c64e53e8e4e
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-12-22 09:49:00 +0530

    address controllers created successfully

--- Backend/src/controllers/address.controllers.js
+++ Backend/src/controllers/address.controllers.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose"
 import { User } from "../models/user.model.js"
 import { ApiError } from "../utils/apiError.js"
 import { ApiResponse }  from "../utils/apiResponse.js"
@@ -7,7 +8,6 @@ import {
     AddressTypeEnum, 
     AvaailableAddressType 
 } from "../utils/constants.js"
-import mongoose from "mongoose"
 
 
 export const createUserAddress = asyncHandler(async(req, res) => {
@@ -57,13 +57,18 @@ export const createUserAddress = asyncHandler(async(req, res) => {
     })
     if(!address) throw new ApiError(401, "address creat to faild")
     
-    return res.status(200).json(new ApiResponse(
-        201,
-        address,
-        "address create successfully"
-    ))
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                201,
+                address,
+            "address create successfully"
+            )
+        )
 
-})
+    }
+)
 
 
 export const getAllUserAddress = asyncHandler(async(req, res) => {
@@ -103,13 +108,16 @@ export const getAllUserAddress = asyncHandler(async(req, res) => {
     ])
     if(!address || address.length == 0 ) throw new ApiError(404, "address is required")
 
-    return res.status(200).json(
-        new ApiResponse(200,
-            address,
-            "address fetched successfully"
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200,
+                address,
+                "address fetched successfully"
+            )
         )
-    )
-})
+    }
+)
 
 
 
@@ -123,13 +131,95 @@ export const  getAddressById = asyncHandler(async(req, res) => {
     })
     if(!address) throw new ApiError(401, "address doew not exist")
     
-    return res.status(200).json(
-        new ApiResponse(200,
-            address,
-            "address fetchech successfully"
-        ) 
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200,
+                address,
+                "address fetchech successfully"
+            ) 
+    )
+})
+
+
+export const addressUpdate = asyncHandler(async(req, res) => {
+    const { addressId } = req.params
+    if(!addressId) throw new ApiError(401, "addressId create to faild")
+
+    const owner = req.user._id;
+    if(!owner) throw new ApiError(401, "owner fetch to faild")
+
+    const {
+        fullName, 
+        phoneNumber,
+        address1,
+        address2,
+        city,
+        state,
+        pineCode,
+        country,
+        addressType,
+        isDefault
+    } = req.body
+
+    const address = await Address.findOneAndUpdate(
+        {
+            _id: addressId,
+            owner: req.user._id
+        },
+        {
+            $set: {
+                fullName, 
+                phoneNumber,
+                address1,
+                address2,
+                city,
+                state,
+                pineCode,
+                country,
+                addressType,
+                isDefault
+            }
+        },
+        {
+            new: true
+        }
+    )
+    if(!address) throw new ApiError(401, "address does not exist")
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                address,
+                "Address updated successfully"
+            )
     )
 
+}) 
+
+export const deleteAddress = asyncHandler(async(req, res) => {
+    const {addressId} = req.params;
+    if(!addressId) throw new ApiError(401, "addressId does not exist")
 
 
-})
+    const address = await Address.findOneAndDelete({
+        _id: addressId,
+        owner: req.user._id
+    })
+    if(!address) throw new ApiError(401, "address does not exist")
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    deleteAddress: address
+                },
+                "Address deleted successfully"
+            )
+        )
+    }
+)
--- Backend/src/routes/address.routes.js
+++ Backend/src/routes/address.routes.js
@@ -2,7 +2,9 @@ import { Router } from "express"
 import { 
     createUserAddress,
     getAllUserAddress,
-    getAddressById
+    getAddressById,
+    addressUpdate,
+    deleteAddress
 } from "../controllers/address.controllers.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -11,5 +13,7 @@ const router = Router()
 router.post("/createAddress/:userId", verifyJWT, createUserAddress)
 router.get("/getAllUserAddress", verifyJWT, getAllUserAddress)
 router.get("/getAddressById/:addressId", verifyJWT, getAddressById)
+router.patch("/addressUpdate/:addressId", verifyJWT, addressUpdate)
+router.delete("/deleteAddress/:addressId", verifyJWT, deleteAddress)
 
 export default router
